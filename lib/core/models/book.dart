0377f6bc5015c8e38442a94c04e388f90332ea1f
NguyenKhanh Nhat <nk_nhat@brycen-vn.local>
2020-03-17 09:31:21 +0700

    .

--- lib/core/models/book.dart
+++ lib/core/models/book.dart
@@ -1,27 +1,26 @@
+import 'package:firebase_database/firebase_database.dart';
+
 class Book {
   String id;
   String title;
   String category;
   String author;
   int chapters;
-  DateTime startdate;
-  DateTime dateupdate;
-  String describe;
-  String status;
-  int like;
-  int rating;
 
   //contructor
-  Book(
-      {this.id,
-      this.title,
-      this.category,
-      this.author,
-      this.chapters,
-      this.startdate,
-      this.dateupdate,
-      this.describe,
-      this.status,
-      this.like,
-      this.rating});
+  Book(this.id, this.title, this.category, this.author, this.chapters);
+
+  Book.fromSnapshot(DataSnapshot snapshot)
+      : id = snapshot.key ?? '',
+        title = snapshot.value["title"] ?? '',
+        category = snapshot.value["category"] ?? '',
+        chapters = snapshot.value["chapters"] ?? 0;
+  toJson() {
+    return {
+      "title": title,
+      "category": category,
+      "author": author,
+      "chapters": chapters
+    };
+  }
 }
